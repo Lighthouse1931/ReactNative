4cfb023499726090ccd939734d7df0d04262a8d2
Cao Hoàng Anh <khaikhai331@gmail.com>
2024-10-05 11:18:18 +0700

    Create ProfileViewFakeData.js
    ok

---
+++ FakeData/ProfileViewFakeData.js
@@ -0,0 +1,12 @@
+const ProfileViewFakeData = [
+    { id: 1, image: 'https://img.icons8.com/color/70/000000/cottage.png', title: 'Order' },
+    {
+        id: 2,
+        image: 'https://img.icons8.com/color/70/000000/administrator-male.png',
+        title: 'Like',
+    },
+    { id: 3, image: 'https://img.icons8.com/color/70/000000/filled-like.png', title: 'Comment' },
+    { id: 4, image: 'https://img.icons8.com/color/70/000000/facebook-like.png', title: 'Download' },
+    { id: 5, image: 'https://img.icons8.com/color/70/000000/shutdown.png', title: 'Edit' },
+];
+export default ProfileViewFakeData;
