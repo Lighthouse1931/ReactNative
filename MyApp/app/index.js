eecd64a7bf49ff3c4988e3c81ab22a120150eba9
Cao Hoàng Anh <khaikhai331@gmail.com>
2024-10-07 16:52:32 +0700

    flexbox + flatlist
    ok

--- MyApp/app/index.js
+++ MyApp/app/index.js
@@ -1,14 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import StackNavigator from '../navigation/StackNavigator'
-import TabNavigator from '../navigation/TabNavigator'
-import DrawerNavigator from '../navigation/DrawerNavigator'
-import TwoFactorAuthView from '../components/TwoFactorAuthView'
+import FriendListScreen from '../components/FriendListScreen'
+import FlexBoxDemo from '../components/FlexBoxDemo'
+
 
 const index = () => {
   return (
-      <TwoFactorAuthView></TwoFactorAuthView>
+    <FlexBoxDemo></FlexBoxDemo>
   )
 }
 
